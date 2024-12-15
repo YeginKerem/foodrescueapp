be6905bff8910a75ac869859e0de9ead3c9a25d1
KEREM <kerem_4355@hotmail.com>
2024-12-15 22:54:41 +0300

    merge

--- foodrescueapp/urls.py
+++ foodrescueapp/urls.py
@@ -16,10 +16,8 @@ urlpatterns = [
     path('create_donation/', views.create_donation_view, name='create_donation_view'),
     path('feedback/', views.feedback, name='feedback'),
     path('logout_request', views.logout_request, name='logout'),
-    path('donate/reservation/<int:donation_id>/', views.create_donation_view, name='reservation'),
     path("admin-panel/", views.admin_panel, name="admin_panel"),  # HTML döndürür
     path("admin-panel/api/", views.admin_user_api, name="admin_user_api"),  # JSON API
     path('admin-panel/donors/', views.admin_donors, name='admin_donors'),
     path('admin-panel/donor-api/', views.admin_donor_api, name='admin_donor_api'),
-
 ]
